74067a1cc56c1c2e41f26a872ec02702e3a649df
Belen-Sosa <sosabelen597@gmail.com>
2024-08-30 15:22:27 -0300

    function JWT created

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,26 +1,41 @@
+import { createAccessToken } from "../libs/jwt.js";
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
-export const register =async (req, res)=>{
 
-   try {
-        const {email,password,username}= req.body;
-        const newUser= new User({
-            username,
-            email,
-            password
-        });
+export const register = async (req, res) => {
+  const { email, password, username } = req.body;
+  //encriptando contraseña
 
-        //guardar nuevo usuario
-       const userSaved= await newUser.save();
-        res.json(userSaved)
-     
+  const passwordHash = await bcrypt.hash(password, 10);
 
-   } catch (error) {
+  try {
+    const newUser = new User({
+      username,
+      email,
+      password: passwordHash,
+    });
+
+    //guardar nuevo usuario
+    const userSaved = await newUser.save();
+   //creando jwt
+    const token = await createAccessToken({id:userSaved._id})
+    
+    //establecer el token en una cookie
+    res.cookie('token',token);
+
+
+   
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updateAt: userSaved.updatedAt,
+    });
+  } catch (error) {
     console.log(error);
-    
-   }
-}
+  }
+};
 
-export const login = (req, res)=>{
-    
-}
+export const login = (req, res) => {};
