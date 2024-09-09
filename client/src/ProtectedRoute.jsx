2841eec449e6600af99afb4f693c048d0b6e7723
Belen-Sosa <sosabelen597@gmail.com>
2024-09-09 18:27:03 -0300

    protecting routes

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -5,8 +5,7 @@ function ProtectedRoute() {
   const { loading, isAuthenticated } = useAuth();
   console.log(loading, isAuthenticated);
 
-  if (loading) return   <h1>Cargando...</h1>;
-
+  if (loading) return <h1>Cargando...</h1>;
 
   if (!loading && !isAuthenticated) return <Navigate to="/login" replace />;
   return <Outlet />;
